29297f74af0beb15dbe2c8b82d43774aecf76846
fktona <113286902+fktona@users.noreply.github.com>
2024-05-13 17:02:03 +0100

    Update front.tsx

--- app/components/front.tsx
+++ app/components/front.tsx
@@ -18,11 +18,11 @@ export default function FrontPage({ show , setShow}: { show: boolean , setShow: 
     <div className={`relative w-full h-[100vh]  flex justify-center ${fade && 'flow'} items-center  bg-[#21121B]`}>
     <div className='flex flex-col gap-[142px]'>
       <div className={`relative w-full h-full front flex justify-center items-center `}>
-        <Image src='/cteddy.svg' width={200} height={200} alt='eye ' className='relative z-10  vlg:w-[500px] vlg:aspect-square'  />
+        <Image src='/cteddy.svg' width={200} height={200} alt='eye ' className='relative z-10  lg:w-[500px] lg:aspect-square'  />
         <Image src='/eye.png' width={200} height={200} alt='eye' className='absolute vlg:w-[500px] vlg:aspect-square '/>
         </div>
         <button onClick={handleShow}
-         className='frontButton lg:px-[50px] py-[20px] lg:py-[6.7px]'>
+         className='frontButton lg:px-[50px] py-[20px] lg:text-[87px] lg:py-[6.7px]'>
             LET ME IN
         </button>
     </div>
