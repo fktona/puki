ac569e7e31a7fbb4b6530e32dd8c7fbdc9691de9
fktona <113286902+fktona@users.noreply.github.com>
2024-05-13 16:55:22 +0100

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      screens: {
+        'vlg': '1600px',
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
