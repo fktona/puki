b034c3fe32f1e9651c9c828d1b4699795aebe147
fktona <adetona.fk@gmail.com>
2024-05-13 11:07:40 -0700

    [completed]

--- app/components/front.tsx
+++ app/components/front.tsx
@@ -16,13 +16,13 @@ export default function FrontPage({ show , setShow}: { show: boolean , setShow: 
   }
   return (
     <div className={`relative w-full h-[100vh]  flex justify-center ${fade && 'flow'} items-center  bg-[#21121B]`}>
-    <div className='flex flex-col gap-[142px]'>
+    <div className='flex flex-col justify-around gap-10'>
       <div className={`relative w-full h-full front flex justify-center items-center `}>
-        <Image src='/cteddy.svg' width={200} height={200} alt='eye ' className='relative z-10  lg:w-[500px] lg:aspect-square'  />
-        <Image src='/eye.png' width={200} height={200} alt='eye' className='absolute vlg:w-[500px] vlg:aspect-square '/>
+        <Image src='/cteddy.svg' width={200} height={200} alt='eye ' className='relative z-10 vlg:w-[500px]  lg:w-[300px] lg:aspect-square'  />
+        <Image src='/eye.png' width={200} height={200} alt='eye' className='absolute lg:w-[300px] vlg:w-[500px] lg:aspect-square '/>
         </div>
         <button onClick={handleShow}
-         className='frontButton lg:px-[50px] py-[20px] lg:text-[87px] lg:py-[6.7px]'>
+         className='frontButton lg:px-[50px] py-[20px] lg:text-[47px] lg:py-[6.7px] vlg:text-[87px]'>
             LET ME IN
         </button>
     </div>
