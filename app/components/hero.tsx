1ed08b47fac88340263facdd89c6ae64a503791b
fktona <adetona.fk@gmail.com>
2024-05-15 07:36:51 -0700

    [completed]

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -11,18 +11,18 @@ export default function Hero() {
     <section className='bgg1'>
     <div ref={homeRef}
      className='relative max-w-[1440px] mx-auto    w-[100%] min-h-[100vh] md:min-h-fit overflow-hidden z-20 '>
-      <div className=' absolute right-[-35%] md:right-[-20px] opacity-100
-       md:w-[65%] lg:w-[60%]   p-5  w-[151%] bottom-[-15px] ] '>
-      <Image src='/teddy.png' width={940} height={940} alt='puki' className='relative 
+      <div className=' absolute right-[-55%] md:right-[-25px] opacity-100
+       md:w-[65%] lg:w-[60%]   p-5  w-[171%] bottom-[-15px] ] '>
+      <Image src='/teddy.svg' width={940} height={940} alt='puki' className='relative 
       heroflow w-full h-full '/>
       </div>
      <Nav />
-     <div className=' relative mt-[50px] z-50  md:pb-[180px] 
+     <div className=' relative mt-[80px] z-50  md:pb-[180px] 
       lg:mt-[50px] md:px-[107px] px-[25px]  text-[60px] '>
         <div className=' font-bold text-white'><p className='md:text-[50px] text-[30px] 
         leading-[87%]'>Welcome To </p>
         <p className='lg:text-[190px] md:text-[140px] text-[110px] leading-[87%] text-[#FFB500]'>pukionsol</p></div>
-        <p className='text-white text-lg lg:mt-[50px] mt-8 text-[29px] md:text-[50px]  '>The baddest bear on the block (chain)</p>
+        <p className='text-white text-lg lg:mt-[50px] mt-8 text-[32px] md:text-[50px]  '>The baddest bear on the block (chain)</p>
         <button className='herobtn lg:px-[50px] bg-[#4E3133] px-[24px] mt-[80px]  text-[35px] md:text-[64px] md:py-[6.7px]'>
         Grab  $PUKI
         </button>
