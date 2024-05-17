0e91412cc3195be133b5e881e1cd90bb651d4caf
fktona <adetona.fk@gmail.com>
2024-05-17 14:23:41 -0700

    [completed]

--- app/grab.tsx
+++ app/grab.tsx
@@ -11,7 +11,7 @@ export default function Grab() {
       flex flex-col items-center justify-between  relative
       lg:space-y-[150px] lg:py-[68px] py-[50px] space-y-[100px]'>
         <h2 className='lg:text-[102px] text-[#F5A435]  text-[45px]'>Get Wild with PUKI</h2>
-        <a href="https://dexscreener.com/solana/GwjR1knoW4KMcBGPKrURwvntqUyRenoaF4YyzwR8VYoy"><button className='herobtn lg:px-[50px] px-[24px] mt-[45px] text-[35px] lg:text-[64px] lg:py-[6.7px]'>
+        <a href="https://dexscreener.com/solana/GwjR1knoW4KMcBGPKrURwvntqUyRenoaF4YyzwR8VYoy" target='_blank'><button className='herobtn lg:px-[50px] px-[24px] mt-[45px] text-[35px] lg:text-[64px] lg:py-[6.7px]'>
         Grab  $PUKI
         </button></a>
      
